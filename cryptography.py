a5e1ec6718fc106b50f54fef779e76ec4e5781b2
Npikielny <42814781+Npikielny@users.noreply.github.com>
2018-09-08 21:24:25 -0400

    Updated from Brython Server: 9/8/2018 9:24:25 PM

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,32 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+charNumb = {}
+numbChar = {}
+for i in range(0, len(associations)):
+    charNumb[associations[i]] = i
+    numbChar[i] = associations[i]
+
+op = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+while op != "e" and op != "d" and op != "q":
+    print("Did not understand command, try again.")
+    op = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+
+while op != "q":
+    if op == "e":
+        m = str(input("Message: "))
+        k = str(input("Key: "))
+        i = 0
+        while len(m) > len(k):
+            key += key[i]
+            i += 1
+            
+    else:
+        print("meh")
+    op = "q"
+    """
+    op = str(input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    while op != "e" and op != "d" and op != "q":
+        print("Did not understand command, try again.")
+        op = str(input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    """
